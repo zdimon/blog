e338059d4de20204520b34d3dc664485bdd0e8af
zdimon77@gmail.com <zdimon77@gmail.com>
2013-11-27 19:48:21 +0200

    first

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def home(request):
+    return render_to_response('home.html', {},context_instance=RequestContext(request))
